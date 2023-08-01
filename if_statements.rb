dd17acfc0951f2e0600741f7ef6032b973366410
Sooyung Kim <kimx1279@gmail.com>
2023-08-01 08:56:05 -0600

    Add second challenge response

--- if_statements.rb
+++ if_statements.rb
@@ -43,9 +43,11 @@ end
 
 num_quarters = 0
 
+if num_quarters >= 2
 puts "I have enough money for a gumball"
+else
 puts "I don't have enough money for a gumball"
-
+end
 
 #####################
 # Using the variables defined below, write code that will tell you
